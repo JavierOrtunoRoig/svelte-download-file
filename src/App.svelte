182fac374811c652d3ea69c4d99841a75e829cbf
Javier Ortuño Roig <javi_roig@hotmail.es>
2022-12-20 19:51:10 +0100

    feat: :tada: base project

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import svelteLogo from "./assets/svelte.svg";
+  import { downloadBlob } from "./helper";
+
+  let file: FileList;
+</script>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <!-- get a file -->
+  <input type="file" bind:files={file} multiple={false} />
+  <br />
+  <br />
+  <button on:click={() => downloadBlob(file[0])}>Descargar</button>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+</style>
